5a39cd15628d6101f7aa628deb6b7eac2a92ebfe
paulkabzz <pkabulu05@gmail.com>
2025-03-24 14:21:53 +0200

    created methods to load file, get balance factor and fix height

--- src/GenericsKbAVLApp.java
+++ src/GenericsKbAVLApp.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.Math;
 import java.util.*;
@@ -33,6 +35,29 @@ public class GenericsKbAVLApp {
 
     private int getChoice() {
         return this.scanner.nextInt();
+    }
+
+    public void loadMainFile(String file) throws IOException {
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Statement statement = Statement.fromLine(line);
+
+            }
+
+        } catch (IOException e) {
+            ConsoleStyle.printError("Error loading file: " + e.getMessage());
+        }
+        
+    }
+
+    private int balanaceFactor(Node node) {
+        return node == null ? 0 : height(node.right) - height(node.left);
+    }
+
+    private void fixHeight(Node node) {
+        node.height = 1 + Math.max(height(node.left), height(node.right));
     }
 
 }
